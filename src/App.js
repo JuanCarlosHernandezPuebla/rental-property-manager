9875dc27989df65be27248fc257c5173e0a43b73
Juan Carlos <juancarloshernandezpuebla@ymail.com>
2018-10-13 23:20:34 -0500

    Add icons to navigation links

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React, {Component} from 'react';
 import Header from './components/Header';
 import Main from './Main';
 import Footer from './components/Footer';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faHome, faBook, faQuestionCircle, faSignInAlt, faStar } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faHome, faStar, faBook, faQuestionCircle, faSignInAlt);
 
 class App extends Component {
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 class Header extends Component {
 
@@ -15,29 +16,30 @@ class Header extends Component {
 
                     <ul className="navbar-nav mr-auto">
                         <li className="nav-item active">
-                            <Link className="nav-link" to='/home'>Home</Link>
+                            <Link className="nav-link" to='/home'><FontAwesomeIcon icon="home" className="mr-2"/>Home</Link>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" to='/features'>Features</Link>
+                            <Link className="nav-link" to='/features'><FontAwesomeIcon icon="star" className="mr-2"/>Features</Link>
                         </li>
                         <li className="nav-item dropdown">
                             <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <FontAwesomeIcon icon="book" className="mr-2"/>
                                 Resources
                                 </a>
                             <div className="dropdown-menu" aria-labelledby="navbarDropdown">
                                 <a className="dropdown-item" href="#">Resource #1</a>
                                 <a className="dropdown-item" href="#">Resource #2</a>
                                 <div className="dropdown-divider"></div>
-                                <a className="dropdown-item" href="#">Sign Up</a>
+                                <Link className="nav-link" to="/create">Create Account</Link>
                             </div>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" to="/help">Help</Link>
+                            <Link className="nav-link" to="/help"><FontAwesomeIcon icon="question-circle" className="mr-2"/>Help</Link>
                         </li>
                     </ul>
                     <ul className="navbar-nav navbar-right">
                         <li className="bold">
-                            <Link className="nav-link" to="/login">Login</Link>
+                            <Link className="nav-link" to="/login">Log in<FontAwesomeIcon icon="sign-in-alt" className="ml-2"/></Link>
                         </li>
                     </ul>
                 </nav>
