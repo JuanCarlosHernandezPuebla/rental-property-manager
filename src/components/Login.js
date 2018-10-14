4942a9afc241776d1168a027d3cae6be22e9d450
Juan Carlos <juancarloshernandezpuebla@ymail.com>
2018-10-13 23:22:34 -0500

    Add login form

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Login extends Component {
 
     render() {
         return(
             <div className="container">
-                <h1>Login</h1>
-                <p>Under Construction...</p>
+                <form className="form-login">
+                    <img className="mb-4" src="/images/rpm_logo.png" alt="" width="72" height="72" />
+                    <h1 className="h3 mb-3 font-weight-normal">Please log in</h1>
+                    <label htmlFor="inputUsername" className="sr-only">Username</label>
+                    <input type="text" id="inputUsername" className="form-control" placeholder="Username" required autoFocus />
+                    <label htmlFor="inputPassword" className="sr-only">Password</label>
+                    <input type="password" id="inputPassword" className="form-control" placeholder="Password" required />
+                    <button className="btn btn-lg btn-primary btn-block btn-login" type="submit">Log in</button>
+                    <div>
+                        <Link className="nav-link" to="/create">Create an account</Link>
+                    </div>
+                </form>
             </div>     
         );
     }
