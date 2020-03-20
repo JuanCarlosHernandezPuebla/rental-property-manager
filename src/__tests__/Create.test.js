6d593b363838eaefd1272b48ad695abdf4f05058
Juan Carlos <juancarloshernandezpuebla@ymail.com>
2020-03-20 03:51:55 -0500

    Update test

--- src/__tests__/Create.test.js
+++ src/__tests__/Create.test.js
@@ -2,8 +2,13 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import Create from '../components/Create';
 import renderToJson from 'enzyme-to-json';
+import { MemoryRouter } from 'react-router'
 
 test('Create render test', () => {
-    const component = shallow(<Create/>);
-    expect(renderToJson(component)).toMatchSnapshot();
+  const component = shallow(
+    <MemoryRouter initialEntries={[{ pathname: '/create', key: 'testKey' }]} >
+      <Create />
+    </MemoryRouter>
+  );
+  expect(renderToJson(component)).toMatchSnapshot();
 });
