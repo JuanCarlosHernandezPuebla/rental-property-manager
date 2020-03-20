4d39f624a92ee6d438fe0ca76ff846bd4a5287db
Juan Carlos <juancarloshernandezpuebla@ymail.com>
2020-03-20 01:00:13 -0500

    Replace with controlled Form component

--- src/components/Create.js
+++ src/components/Create.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Modal } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import SignupForm from './Forms/SignupForm';
 
 const createAccountEndPoint = 'http://localhost:3000/users/create';
 
@@ -58,18 +59,10 @@ export default function Create() {
 
   return (
     <div className="container">
-      <form className="form-signup" method="POST" onSubmit={createAccount}>
-        <img className="mb-4" src="/images/rpm_logo.png" alt="" width="72" height="72" />
-        <h1 className="h3 mb-3 font-weight-normal">Create an account</h1>
-        <label htmlFor="input_username">Username</label>
-        <input type="text" id="input_username" name="username" className="form-control" placeholder="Username" required autoFocus />
-        <label htmlFor="input_password">Password</label>
-        <input type="password" id="input_password" name="password" className="form-control" placeholder="Password" required />
-        <button className="btn btn-lg btn-primary btn-block btn-login" type="submit">Create an account</button>
-        <div>
-          <Link className="nav-link" to="/login">Log Into Existing Account</Link>
-        </div>
-      </form>
+      <SignupForm onSubmit={createAccount} />
+      <div className="text-center">
+        <Link className="nav-link" to="/login">Log Into Existing Account</Link>
+      </div>
       <Modal
         show={modal.show}
         centered
